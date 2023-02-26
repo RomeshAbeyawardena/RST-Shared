40563caf1b9e73816267a13045b180505a9c5ed6
romeshabeyawardena <RomeshAbeyawardena@users.noreply.github.com>
2023-02-26 19:39:47 +0000

    Made paged rsult public

--- RST/Defaults/DefaultPagedResult.cs
+++ RST/Defaults/DefaultPagedResult.cs
@@ -3,25 +3,46 @@ using System.Collections;
 
 namespace RST.Defaults;
 
-internal class DefaultPagedResult<TResult> : DefaultPagedResult<int, TResult>, IPagedResult<TResult>
+/// <summary>
+/// Represents a default paged result
+/// </summary>
+/// <typeparam name="TResult"></typeparam>
+public class DefaultPagedResult<TResult> : DefaultPagedResult<int, TResult>, IPagedResult<TResult>
 {
 
 }
 
-internal class DefaultPagedResult<T, TResult> : DefaultResult<IEnumerable<T>>, IPagedResult<T, TResult>
+/// <summary>
+/// Represents a default paged result
+/// </summary>
+/// <typeparam name="T"></typeparam>
+/// <typeparam name="TResult"></typeparam>
+public class DefaultPagedResult<T, TResult> : DefaultResult<IEnumerable<T>>, IPagedResult<T, TResult>
     where T : struct
 {
+    /// <summary>
+    /// Initializes an instance of <see cref="DefaultPagedResult{T, TResult}"/> 
+    /// </summary>
     public DefaultPagedResult()
     {
         Results = new List<TResult>();
     }
 
+    ///<inheritdoc />
     public T PageNumber { get; set; }
+
+    ///<inheritdoc />
     public T NumberOfPages { get; set; }
+
+    ///<inheritdoc />
     public T TotalItems { get; set; }
+
+    ///<inheritdoc />
     public IEnumerable<TResult> Results { get; set; }
+    
     IEnumerable<TResult>? IResult<IEnumerable<TResult>>.Value => Results;
 
+    ///<inheritdoc />
     public IEnumerator<TResult> GetEnumerator()
     {
         return Results.GetEnumerator();
