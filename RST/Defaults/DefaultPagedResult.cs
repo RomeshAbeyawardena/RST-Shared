2539a1760806941a482522002f0ba262ca482ddb
romeshabeyawardena <RomeshAbeyawardena@users.noreply.github.com>
2023-02-26 21:06:19 +0000

    Fixed an issue with underlining result type

--- RST/Defaults/DefaultPagedResult.cs
+++ RST/Defaults/DefaultPagedResult.cs
@@ -17,7 +17,7 @@ public class DefaultPagedResult<TResult> : DefaultPagedResult<int, TResult>, IPa
 /// </summary>
 /// <typeparam name="T"></typeparam>
 /// <typeparam name="TResult"></typeparam>
-public class DefaultPagedResult<T, TResult> : DefaultResult<IEnumerable<T>>, IPagedResult<T, TResult>
+public class DefaultPagedResult<T, TResult> : DefaultResult<IEnumerable<TResult>>, IPagedResult<T, TResult>
     where T : struct
 {
     /// <summary>
