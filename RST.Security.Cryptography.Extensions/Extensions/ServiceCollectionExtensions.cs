3452d989580312e870886cd23a0f895fce8cc89b
romeshabeyawardena <RomeshAbeyawardena@users.noreply.github.com>
2023-03-04 19:24:32 +0000

    Added tests

--- RST.Security.Cryptography.Extensions/Extensions/ServiceCollectionExtensions.cs
+++ RST.Security.Cryptography.Extensions/Extensions/ServiceCollectionExtensions.cs
@@ -13,19 +13,22 @@ public static class ServiceCollectionExtensions
     /// <summary>
     /// Adds the encryption module option to the provided <paramref name="services"/> parameter
     /// </summary>
-    /// <param name="services"></param>
-    /// <param name="encryptionCaseConvention"></param>
-    /// <param name="configureEncryptionOptions"></param>
+    /// <param name="services">Instance of <see cref="IServiceCollection"/> to register the encryption module options to</param>
+    /// <param name="encryptionCaseConvention">Default encryption case convention</param>
+    /// <param name="configureEncryptionOptions">Configures encryption dictionary</param>
+    /// <param name="configureServiceEncryptionOptions">Configures encryption factory dictionary</param>
     /// <returns></returns>
     public static IServiceCollection AddEncyptionModuleOption(this IServiceCollection services,
-        EncryptionCaseConvention encryptionCaseConvention,
-        Action<IDictionaryBuilder<string, IEncryptionOptions>>? configureEncryptionOptions = null)
+        EncryptionCaseConvention encryptionCaseConvention = EncryptionCaseConvention.Default,
+        Action<IDictionaryBuilder<string, IEncryptionOptions>>? configureEncryptionOptions = null,
+        Action<IDictionaryBuilder<string, Func<IServiceProvider, IEncryptionOptions>>>? configureServiceEncryptionOptions = null)
     {
         return services.AddSingleton<IEncryptionModuleOptions, DefaultEncryptionModuleOptions>(
             (services) =>
         {
             var options = new DefaultEncryptionModuleOptions(encryptionCaseConvention);
             configureEncryptionOptions?.Invoke(new DefaultDictionaryBuilder<string, IEncryptionOptions>(options.EncryptionOptions));
+            configureServiceEncryptionOptions?.Invoke(new DefaultDictionaryBuilder<string, Func<IServiceProvider, IEncryptionOptions>>(options.EncryptionOptionsFactory));
             return options;
         });
     }
---
+++ RST.UnitTests/DictionaryBuilderTests.cs
@@ -0,0 +1,25 @@
+﻿using RST.Defaults;
+
+namespace RST.UnitTests;
+
+[TestFixture]
+public class DictionaryBuilderTests
+{
+    private Dictionary<string, int> underliningDictionary;
+    private DefaultDictionaryBuilder<string, int> dictionaryBuilder;
+    [SetUp]
+    public void SetUp()
+    {
+        underliningDictionary= new Dictionary<string, int>();
+        dictionaryBuilder = new DefaultDictionaryBuilder<string, int>(underliningDictionary);
+    }
+
+
+    [Test]
+    public void Ensure_underlining_dictionary_is_updated()
+    {
+        dictionaryBuilder.Add("banana", 1234);
+
+        Assert.That(dictionaryBuilder, Does.Contain(KeyValuePair.Create("banana", 1234)));
+    }
+}
