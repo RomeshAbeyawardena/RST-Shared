b49666c744373d6db800aa6264f13830779aa333
romeshabeyawardena <RomeshAbeyawardena@users.noreply.github.com>
2023-03-05 16:28:18 +0000

    Added methods

--- RST.Extensions.Configuration/ConfigurationExtensions.cs
+++ RST.Extensions.Configuration/ConfigurationExtensions.cs
@@ -9,6 +9,17 @@ namespace RST.Extensions.Configuration;
 public static class ConfigurationExtensions
 {
     /// <summary>
+    /// Gets a list of <see cref="IConfigurationSection"/> instances from the supplied <paramref name="paths"/> from supplied <paramref name="configuration"/> 
+    /// </summary>
+    /// <param name="configuration">Configuration instance</param>
+    /// <param name="paths">Paths to extract from <see cref="IConfiguration"/></param>
+    /// <returns></returns>
+    public static IEnumerable<IConfigurationSection> GetSectionsFromPath(this IConfiguration configuration, params string[] paths)
+    {
+        return paths.Select(p => GetFromPath(configuration, p));
+    }
+
+    /// <summary>
     /// Traverses through the sections within <paramref name="configuration"/> in order find an instance of <see cref="IConfigurationSection"/> by a given path separated by either a '/' or '\\'
     /// </summary>
     /// <param name="configuration"><see cref="IConfiguration"/> instance to obtain <see cref="IConfigurationSection"/></param>
