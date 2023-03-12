76c6dd62d10bf68f651642ff5ecba98acfb84178
romeshabeyawardena <RomeshAbeyawardena@users.noreply.github.com>
2023-03-12 22:19:40 +0000

    Fixed xml documentation errors

--- RST.Extensions.Configuration/ConfigurationExtensions.cs
+++ RST.Extensions.Configuration/ConfigurationExtensions.cs
@@ -18,7 +18,7 @@ public static class ConfigurationExtensions
         var rootSection = configuration.GetFromPath(rootPath);
         return rootSection.GetChildren();
     }
-    ///public static IEnumerable<IConfigurationSection>
+    
     /// <summary>
     /// <inheritdoc cref="ConfigurationBinder.Get{T}(IConfiguration)"/>
     /// </summary>
