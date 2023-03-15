1ca9d574d059dd76449eb8ca7f3f3a05743d9eb7
romeshabeyawardena <RomeshAbeyawardena@users.noreply.github.com>
2023-03-15 21:37:12 +0000

    Added ApplicationAuthenticationSchemeOptions.cs

--- RST.AspNetCore.Extensions/ApplicationAuthentication.cs
+++ RST.AspNetCore.Extensions/ApplicationAuthentication.cs
@@ -3,70 +3,73 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using RST.AspNetCore.Extensions.Contracts;
 using RST.Contracts;
-using System.Buffers.Text;
 using System.Security.Claims;
 using System.Text.Encodings.Web;
 
 namespace RST.AspNetCore.Extensions
 {
-    internal class ApplicationAuthenticationSchemeOptions : AuthenticationSchemeOptions
-    {
-        public ApplicationAuthenticationSchemeOptions()
-        {
-
-        }
-
-        public Type IdentityType { get; set; }
-
-        public override void Validate()
-        {
-            base.Validate();
-        }
-
-        public override void Validate(string scheme)
-        {
-            base.Validate(scheme);
-        }
-    }
-    internal class ApplicationAuthentication : AuthenticationHandler<ApplicationAuthenticationSchemeOptions>
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ApplicationAuthentication : AuthenticationHandler<ApplicationAuthenticationSchemeOptions>
     {
         private readonly IApplicationAuthenticationRepository applicationAuthenticationRepository;
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="options"></param>
+        /// <param name="logger"></param>
+        /// <param name="encoder"></param>
+        /// <param name="clock"></param>
+        /// <param name="securitySignatureProvider"></param>
+        /// <param name="applicationAuthenticationRepository"></param>
         public ApplicationAuthentication(IOptionsMonitor<ApplicationAuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock,
             ISecuritySignatureProvider securitySignatureProvider, IApplicationAuthenticationRepository applicationAuthenticationRepository) : base(options, logger, encoder, clock)
         {
             this.applicationAuthenticationRepository = applicationAuthenticationRepository;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
         {
-            var authorisationToken = Request.Headers.Authorization.FirstOrDefault();
-            if (string.IsNullOrWhiteSpace(authorisationToken) || authorisationToken.Length < 32)
+            try
             {
-                return AuthenticateResult.Fail(new NullReferenceException());
+                var authorisationToken = Request.Headers.Authorization.FirstOrDefault();
+                if (string.IsNullOrWhiteSpace(authorisationToken) || authorisationToken.Length < 32)
+                {
+                    throw new NullReferenceException();
+                }
+
+                var publicKey = authorisationToken.Substring(authorisationToken.Length - 16, 16);
+
+                var identity = await applicationAuthenticationRepository.GetIdentity(publicKey);
+
+                if (identity == null)
+                {
+                    throw new NullReferenceException();
+                }
+
+                var roles = await applicationAuthenticationRepository.GetRoles(identity);
+
+                if (roles == null)
+                {
+                    throw new NullReferenceException();
+                }
+
+                var claims = roles.Select(s => new Claim(s.Key, s.Value));
+
+                return AuthenticateResult.Success(
+                    new AuthenticationTicket(new ClaimsPrincipal(
+                        new ClaimsIdentity(identity, claims)), ""));
             }
-
-            var publicKey = authorisationToken.Substring(authorisationToken.Length - 16, 16);
-
-            var identity = await applicationAuthenticationRepository.GetIdentity(publicKey);
-
-            if (identity == null)
+            catch(NullReferenceException ex)
             {
-                return AuthenticateResult.Fail(new NullReferenceException());
+                return AuthenticateResult.Fail(ex);
             }
-
-            var roles = await applicationAuthenticationRepository.GetRoles(identity);
-
-            if (roles == null)
-            {
-                return AuthenticateResult.Fail(new NullReferenceException());
-            }
-
-            var claims = roles.Select(s => new Claim(s.Key, s.Value));
-
-            return AuthenticateResult.Success(
-                new AuthenticationTicket(new ClaimsPrincipal(
-                    new ClaimsIdentity(identity, claims)), ""));
         }
     }
 }
---
+++ RST.AspNetCore.Extensions/ApplicationAuthenticationSchemeOptions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace RST.AspNetCore.Extensions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ApplicationAuthenticationSchemeOptions : AuthenticationSchemeOptions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public ApplicationAuthenticationSchemeOptions()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void Validate()
+        {
+            base.Validate();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="scheme"></param>
+        public override void Validate(string scheme)
+        {
+            base.Validate(scheme);
+        }
+    }
+}
