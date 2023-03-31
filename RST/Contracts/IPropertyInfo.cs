1e1c780b262194120b91653a18c664c9aa431856
romesh-abeyawardena <romesh.abeyawardena@bellrockgroup.co.uk>
2023-03-31 16:01:16 +0100

    Added property info abstraction

---
+++ RST/Contracts/IPropertyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+
+namespace RST.Contracts;
+
+/// <summary>
+/// Represents a <see cref="System.Type"/> property
+/// </summary>
+public interface IPropertyInfo
+{
+    /// <summary>
+    /// Referenced type this property belongs to
+    /// </summary>
+    Type Type { get; }
+    /// <summary>
+    /// The underlining <see cref="PropertyInfo"/>
+    /// </summary>
+    PropertyInfo Property { get; }
+    /// <summary>
+    /// The related descendant of the <see cref="System.Type"/>
+    /// </summary>
+    Type? RelatedType { get; }
+}
---
+++ RST/Defaults/DefaultPropertyInfo.cs
@@ -0,0 +1,29 @@
+﻿using RST.Contracts;
+using System.Reflection;
+
+namespace RST.Defaults;
+
+/// <summary>
+/// <inheritdoc cref="DefaultPropertyInfo"/>
+/// </summary>
+internal record DefaultPropertyInfo : IPropertyInfo
+{
+    public DefaultPropertyInfo(Type type, PropertyInfo propertyInfo, Type? relatedProperty = null)
+    {
+        Type = type;
+        Property = propertyInfo;
+        RelatedType = relatedProperty;
+    }
+    /// <summary>
+    /// <inheritdoc cref="IPropertyInfo.Type"/>
+    /// </summary>
+    public Type Type { get; }
+    /// <summary>
+    /// <inheritdoc cref="IPropertyInfo.Property"/>
+    /// </summary>
+    public PropertyInfo Property { get; }
+    /// <summary>
+    /// <inheritdoc cref="IPropertyInfo.RelatedType"/>
+    /// </summary>
+    public Type? RelatedType { get; }
+}
