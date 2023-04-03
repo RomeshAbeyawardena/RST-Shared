9a0d372d9766119ceb8cc187ee0b2da75da1b0a7
romesh-abeyawardena <romesh.abeyawardena@bellrockgroup.co.uk>
2023-04-03 10:25:32 +0100

    Added extensions

---
+++ RST/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,50 @@
+﻿namespace RST.Extensions;
+
+/// <summary>
+/// 
+/// </summary>
+public static class EnumerableExtensions
+{
+    /// <summary>
+    /// <inheritdoc cref="List{TResult}.ForEach(Action{TResult})"/>
+    /// </summary>
+    /// <typeparam name="TRequest"></typeparam>
+    /// <typeparam name="TResult"></typeparam>
+    /// <param name="items"></param>
+    /// <param name="itemAction"></param>
+    /// <returns></returns>
+    public static IEnumerable<TResult> ForEach<TRequest, TResult>(this IEnumerable<TRequest> items,
+        Func<int, TRequest, TResult> itemAction)
+    {
+        var resultList = new List<TResult>();
+        for (int i = 0; i < items.Count(); i++)
+        {
+            resultList.Add(
+                itemAction(i, items.ElementAt(i)));
+        }
+
+        return resultList;
+    }
+
+    /// <summary>
+    /// <inheritdoc cref="List{TResult}.ForEach(Action{TResult})"/>
+    /// </summary>
+    /// <typeparam name="TRequest"></typeparam>
+    /// <typeparam name="TResult"></typeparam>
+    /// <param name="items"></param>
+    /// <param name="itemAction"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    public static async Task<IEnumerable<TResult>> ForEach<TRequest, TResult>(this IEnumerable<TRequest> items,
+        Func<int, TRequest, CancellationToken, Task<TResult>> itemAction, CancellationToken cancellationToken)
+    {
+        var resultList = new List<TResult>();
+        for (int i = 0; i < items.Count(); i++)
+        {
+            resultList.Add(await
+                itemAction(i, items.ElementAt(i), cancellationToken));
+        }
+
+        return resultList;
+    }
+}
