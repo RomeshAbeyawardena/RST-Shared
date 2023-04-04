8c772926dcb739455976ab950fee518df0938440
romeshabeyawardena <RomeshAbeyawardena@users.noreply.github.com>
2023-04-04 19:40:05 +0100

    Added .FluentValidation.Extensions

---
+++ RST.FluentValidation.Extensions/FluentValidationRequestHandler.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RST.FluentValidation.Extensions;
+
+/// <summary>
+/// 
+/// </summary>
+public class FluentValidationRequestHandler<TRequest>
+    : MediatR.Pipeline.IRequestPreProcessor<TRequest>
+    where TRequest:notnull
+{
+    private readonly IValidator<TRequest>? validationContext;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public FluentValidationRequestHandler(IServiceProvider serviceProvider)
+    {
+        this.validationContext = serviceProvider.GetService<IValidator<TRequest>>();
+    }
+
+    /// <inheritdoc cref="MediatR.Pipeline.IRequestPreProcessor{TRequest}"/>
+    public async Task Process(TRequest request, CancellationToken cancellationToken)
+    {
+        if(validationContext == null)
+        {
+            return;
+        }
+
+        var validationResults = await validationContext.ValidateAsync(request, cancellationToken);
+        if(!validationResults.IsValid)
+        {
+            throw new ValidationException(validationResults.Errors);
+        }
+    }
+}
---
+++ RST.FluentValidation.Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using MediatR.Pipeline;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace RST.FluentValidation.Extensions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="assemblies"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddValidationHandler(
+            this IServiceCollection services,
+            params Assembly[] assemblies)
+        {
+            return services
+                .AddValidatorsFromAssemblies(assemblies)
+                .AddSingleton(typeof(IRequestPreProcessor<>), typeof(FluentValidationRequestHandler<>));
+        }
+    }
+}
